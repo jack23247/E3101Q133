a1be7ebd203265d63b93dbf90739c3cb062e7372
Mattia Sgro <mattia.sgro@live.it>
2020-01-30 18:00:19 +0100

    some work on sparse matrix converted 4spaces-tabs to tabs character

--- sparse.hpp
+++ sparse.hpp
@@ -1,107 +1,103 @@
-#pragma once
-/**
- * \file SparseMatrix.hpp
- * Una classe template che implementa una matrice sparsa.
- */
+#ifndef SPARSE_HPP
+#define SPARSE_HPP
 
 #include <limits> // Definisce UINT_MAX
 #include <iostream>
 
 /** \def EXC_INDEX_BOUND
- * Eccezione generata in lettura/scrittura fuori dai limiti.
- */
+* Eccezione generata in lettura/scrittura fuori dai limiti.
+*/
 #define EXC_INDEX_BOUNDS 0x01
 
 /** \typedef uint_t
  * Shortcut per unsigned int.
- */
+*/
 typedef unsigned int uint_t;
 
 /** \class SparseMatrix
  * Classe template che implementa una matrice sparsa.
+ * 
+ * @brief Matrice Sparsa
+ * 
+ * @param T tipo del dato
  */
 template <class T> class SparseMatrix {
 
 private:
 
-    /** \class SMNode
-     * Nodo della lista tramite cui la matrice è implementata.
-     */
-    struct SMNode {
-        uint_t x, y;
-        SMNode* next;
-        T data;
+	/** \struct SMNode
+	 * Nodo della lista tramite cui la matrice è implementata.
+	 *  @brief Nodo della matrice sparsa
+	 */
+	struct SMNode {
+		T data;
+		SMNode* next;
+		uint_t x, y;
 
-        SMNode(T data, uint_t x, uint_t y) {
-            this->x = x;
-            this->y = y;
-            this->data = data;
-            this->next = nullptr;
-        }
+		SMNode(T data, uint_t x, uint_t y) {
+			this->x = x;
+			this->y = y;
+			this->data = data;
+			this->next = nullptr;
+		}
 
-        ~SMNode(void) {
-            this->next = nullptr;
-        }
+		~SMNode(void) {
+			this->next = nullptr;
+		}
 
-        inline bool hasNext() { return(this->next != nullptr); }
-        
-        /** TODO: implementare una struct della forma struct element{
-        *           posx;
-        *           posy;
-        *           value;
-        *       };
-        */
-    };
+		inline bool hasNext() { return(this->next != nullptr); }
+	};
 
-    // Dimensioni della matrice
-    uint_t maxX;
-    uint_t maxY; // Linear size x*y
+	// Dimensioni della matrice
+	uint_t maxX;
+	uint_t maxY; // Linear size x*y
 
-    // Testa della lista linkata sottostante
-    SMNode* head;
+	// Testa della lista linkata sottostante
+	SMNode* head;
 
 public:
 
-    // Costruttore
-    SparseMatrix(uint_t maxX, uint_t maxY) {
-        this->head = nullptr;
-        this->maxX = maxX;
-        this->maxY = maxY;
-    }
+	// Costruttore
+	SparseMatrix(uint_t maxX, uint_t maxY) {
+		this->head = nullptr;
+		this->maxX = maxX;
+		this->maxY = maxY;
+	}
 
-    /*
-    * Metodi
-    */
+	/*
+	* Metodi
+	*/
 
-    // add(elem, i, j)
-    // Aggiunge un elemento elem in posizione i,j
-    // Sovrascrive se già presente
-    void add(T elem, uint_t i, uint_t j) {
-        // oob check
-        if(i > this->maxX || j > this->maxY) {
-            throw EXC_INDEX_BOUNDS; // TODO gestione eccezioni
-        }
-        SMNode newNode(elem, i, j);
-        // emptycheck
-        if(this->head == nullptr) {
-            this->head = &newNode;
-        }
-        SMNode* curNodePtr = this->head;
-        // row select
-        while(curNodePtr->hasNext() && curNodePtr->next->x < i) {
-                curNodePtr = curNodePtr->next;
-        }
-        // col select
-        while(
-            curNodePtr->hasNext() &&
-            curNodePtr->next->x == i &&
-            curNodePtr->next->y < j
-            ) {
-                curNodePtr = curNodePtr->next;
-        }
-        // Debug outcome check
-        std::cout << "Aggiungo: <" << i << "," << j << "> in posizione <" << curNodePtr->x << "," << curNodePtr->y << ">" << std::endl;
+	// add(elem, i, j)
+	// Aggiunge un elemento elem in posizione i,j
+	// Sovrascrive se già presente
+	void add(T elem, uint_t i, uint_t j) {
+		// oob check
+		if(i > this->maxX || j > this->maxY) {
+			throw EXC_INDEX_BOUNDS; // TODO gestione eccezioni
+		}
+		SMNode newNode(elem, i, j);
+		// emptycheck
+		if(this->head == nullptr) {
+			this->head = &newNode;
+		}
+		SMNode* curNodePtr = this->head;
+		// row select
+		while(curNodePtr->hasNext() && curNodePtr->next->x < i) {
+				curNodePtr = curNodePtr->next;
+		}
+		// col select
+		while(
+			curNodePtr->hasNext() &&
+			curNodePtr->next->x == i &&
+			curNodePtr->next->y < j
+			) {
+				curNodePtr = curNodePtr->next;
+		}
+		// Debug outcome check
+		std::cout << "Aggiungo: <" << i << "," << j << "> in posizione <" << curNodePtr->x << "," << curNodePtr->y << ">" << std::endl;
 
-    }
+	}
 
 };
+#endif
