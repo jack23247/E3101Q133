7321850e47580860e548f02d937618586cee8bba
Mattia Sgro <mattia.sgro@live.it>
2020-01-31 21:17:44 +0100

    better spaces

--- sparse.hpp
+++ sparse.hpp
@@ -61,10 +61,10 @@ private:
         *           value;
         *       };
         * @jack23247 IMHO annidiamo troppo
-		* @sgro Allora dobbiamo generarla nel return? oppure creiamo una classe
-		* esterna?
-		*/
-	};
+        * @sgro Allora dobbiamo generarla nel return? oppure creiamo una classe
+        * esterna?
+        */
+    };
 
     // Spazio utilizzato
     uint_t size; /** TODO: Refactoring, nome migliore? */
@@ -73,71 +73,71 @@ private:
     uint_t maxX;
     uint_t maxY;
 
-	T def_elem;
-	// Testa della lista linkata sottostante
-	SMNode* head;
+    T def_elem;
+    // Testa della lista linkata sottostante
+    SMNode* head;
 
 public:
-	/** Costruttore per una matrice X*Y senza elementi
-	 *
-	 * @param maxX X della matrice
-	 * @param maxY Y della matrice
-	*/
-	SparseMatrix(uint_t maxX, uint_t maxY, const T &def_elem) {
-		this->head = nullptr;
-		this->size = 0;
-		this->maxX = maxX;
-		this->maxY = maxY;
-		this->def_elem = def_elem;
-	}
+    /** Costruttore per una matrice X*Y senza elementi
+     *
+     * @param maxX X della matrice
+     * @param maxY Y della matrice
+    */
+    SparseMatrix(uint_t maxX, uint_t maxY, const T &def_elem) {
+        this->head = nullptr;
+        this->size = 0;
+        this->maxX = maxX;
+        this->maxY = maxY;
+        this->def_elem = def_elem;
+    }
 
-	/*
-	* Metodi
-	*/
+    /*
+    * Metodi
+    */
 
-	// add(elem, i, j)
-	// Aggiunge un elemento elem in posizione i,j
-	// Sovrascrive se già presente
-	void add(T elem, uint_t i, uint_t j) {
-		// oob check
-		if(i > this->maxX || j > this->maxY) {
-			throw EXC_INDEX_BOUNDS; /** TODO: gestione eccezioni */
-		}
-		SMNode newNode(elem, i, j);
-		// emptycheck
-		if(this->head == nullptr) {
-			this->head = &newNode;
-			this->size +=1;
-		}
-		SMNode* curNodePtr = this->head;
-		// row select
-		while(curNodePtr->hasNext() && curNodePtr->next->x < i) {
-				curNodePtr = curNodePtr->next;
-		}
-		// col select
-		while(
-			curNodePtr->hasNext() &&
-			curNodePtr->next->x == i &&
-			curNodePtr->next->y < j
-			) {
-				curNodePtr = curNodePtr->next;
-		}
-		#ifdef SF_DEBUG
-		// Debug outcome check
-		std::cout << "[SF_DEBUG] Aggiungo: <" << i << "," << j << "> in posizione <" << curNodePtr->x << "," << curNodePtr->y << ">" << std::endl;
+    // add(elem, i, j)
+    // Aggiunge un elemento elem in posizione i,j
+    // Sovrascrive se già presente
+    void add(T elem, uint_t i, uint_t j) {
+        // oob check
+        if(i > this->maxX || j > this->maxY) {
+            throw EXC_INDEX_BOUNDS; /** TODO: gestione eccezioni */
+        }
+        SMNode newNode(elem, i, j);
+        // emptycheck
+        if(this->head == nullptr) {
+            this->head = &newNode;
+            this->size +=1;
+        }
+        SMNode* curNodePtr = this->head;
+        // row select
+        while(curNodePtr->hasNext() && curNodePtr->next->x < i) {
+                curNodePtr = curNodePtr->next;
+        }
+        // col select
+        while(
+            curNodePtr->hasNext() &&
+            curNodePtr->next->x == i &&
+            curNodePtr->next->y < j
+            ) {
+                curNodePtr = curNodePtr->next;
+        }
+        #ifdef SF_DEBUG
+        // Debug outcome check
+        std::cout << "[SF_DEBUG] Aggiungo: <" << i << "," << j << "> in posizione <" << curNodePtr->x << "," << curNodePtr->y << ">" << std::endl;
         #endif
-		/** TODO: inserimento */
-	}
+        /** TODO: inserimento */
+    }
 
 
-	T getByPosition(uint_t i, uint_t j) {
-		// iteratore che scorre la sparse matrix fino all'indice ij, una volta trovato, returna una struct con l'elemento cercato e le coordinate.
-		// in caso l'iteratore arrivi a fine matrice, returna una struct senza indici (o indici -1 -1 ) con l'elemento di default
-		return this->def_elem;
-	}
+    T getByPosition(uint_t i, uint_t j) {
+        // iteratore che scorre la sparse matrix fino all'indice ij, una volta trovato, returna una struct con l'elemento cercato e le coordinate.
+        // in caso l'iteratore arrivi a fine matrice, returna una struct senza indici (o indici -1 -1 ) con l'elemento di default
+        return this->def_elem;
+    }
 
-	void printSM(SparseMatrix<T> sm);
-	// printa su STD::COUT la matrice in ordine, scorrendo per righe.
+    void printSM(SparseMatrix<T> sm);
+    // printa su STD::COUT la matrice in ordine, scorrendo per righe.
 };
 
 #endif
